5d80d8fe4024555475b0ad823906a33ea07e6f1b
wssamfm <100078066+wssamfm@users.noreply.github.com>
2025-04-15 07:40:53 +0300

    app.py

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def attack():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+app.run(host='0.0.0.0', port=port)
